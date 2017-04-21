955fa443abdc9defe51131972acca7840c7e2288
Clamarque <chabox59@gmail.com>
2017-04-21 14:11:21 +0200

    Adding a shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule, FormsModule, MaterialModule
+  ],
+  exports: [ CommonModule, FormsModule, MaterialModule]
+})
+export class SharedModule { }
