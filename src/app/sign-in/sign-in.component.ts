9f5470305272202163cfee43b12d7568e4958e6f
clamarque <chabox59@gmail.com>
2017-08-28 23:21:44 +0200

    Remove unused functions

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -8,28 +8,10 @@ import { AuthService } from '../shared/index';
   templateUrl: './sign-in.component.html'
 })
 export class SignInComponent {
-
   error: string;
 
   constructor(private router: Router, private authService: AuthService, private snackbar: MdSnackBar) { }
-
-  onSubmit(formData) {
-    this.authService.signIn(formData.email, formData.password, (error) => {
-      if (error) {
-        this.error = error
-        this.snackbar.open(this.error, 'hide', { duration: 10000 })
-      }
-      else {
-        this.authService.readUser().subscribe(authData => {
-          if (authData) {
-            this.snackbar.open('Welcome ' + authData.displayName + ' !', '', { duration: 5000 })
-            this.router.navigate(['/movies/now-playing'])
-          }
-        })
-      }
-    })
-  }
-
+   
   login(name: string) {
     this.authService.signInAccount(name, (error) => {
       if (error) {
