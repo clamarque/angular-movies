3f94372fcc79850bc9025c71c16c48473dc23979
clamarque <chabox59@gmail.com>
2017-08-28 23:21:21 +0200

    cache scss and img

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,8 +3,9 @@ module.exports = {
     'dist/**.html',
     'dist/**.js',
     'dist/**.css',
-    'dist/assets/images/*',
-    'dist/assets/icons/*'
+    'dist/assets/icons/*',
+    'dist/assets/images/no_poster_available.jpg',
+    'dist/assets/scss/*'
   ],
   root: 'dist',
   stripPrefix: 'dist/',
