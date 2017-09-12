5885f5ddb42b89ea7c6d5cafeb92403d80f55f6d
clamarque <chabox59@gmail.com>
2017-09-12 23:16:59 +0200

    Adding function return to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 import { Router } from '@angular/router';
 import { AuthService } from './shared/index';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  movieSearching: any[];
+    movieSearching: any[];
     isConnected: boolean = false;
     color = 'primary';
-    
+
     constructor(private authService: AuthService, private router: Router, private snackbar: MdSnackBar) { }
+
+    @HostListener('window:scroll', ['$event']) scrollHandler(event) {
+        const number = window.scrollY;
+        let el = document.getElementById('return-to-top');
+        if (number >= 50) {
+            el.className = "show";
+
+        } else {
+            el.className = "hide";
+        }
+    }
+
+    scrollTop() {
+        window.scrollTo(0, 0);
+    }
 
     searchMovie(term: string) {
         if (term === '') {
             this.router.navigate(['/movies/now-playing']);
         }
         else {
-            this.router.navigate(['/search', { term: term}]);
+            this.router.navigate(['/search', { term: term }]);
         }
     }
     onSignOut() {
@@ -28,7 +43,7 @@ export class AppComponent {
         this.router.navigate(['/movies/now-playing'])
     }
     ngOnInit() {
-      return this.authService.isLoggedIn().subscribe(
+        return this.authService.isLoggedIn().subscribe(
             authStatus => {
                 console.log('connected:', authStatus)
                 if (authStatus == true) return this.isConnected = true
